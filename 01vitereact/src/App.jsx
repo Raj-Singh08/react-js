cb4e6f551a91a562ec18fb89b763ac05caab7cf0
Raj-Singh08 <vimal38051@gmail,com>
2025-05-23 00:32:25 +0530

    Initial commit

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import Chai from './chai'
+
+
+function App() {
+  const username = "Raj";
+  return (
+     <>     
+     <h1>Hii {username}</h1>         
+      <Chai />
+    </>
+  )
+}
+
+export default App
